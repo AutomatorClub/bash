e7de21d07dacb24748612f00b99e61e38b8179c7
pikamar <mareks.pikalovs@gmail.com>
2025-03-31 14:47:11 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo "Start custom bash configuration setup"
 
 MY_BASH_REPO_DIR="~/.mybash"
 
-git clone https://github.com/AutomatorClub/bash.git "${MY_BASH_REPO_DIR}"
+git clone https://github.com/AutomatorClub/bash.git ${MY_BASH_REPO_DIR}
 
 grep -qxF "export MY_BASH_REPO_DIR=${MY_BASH_REPO_DIR}" ~/.bashrc || echo "export MY_BASH_REPO_DIR=${MY_BASH_REPO_DIR}" >> ~/.bashrc
 
